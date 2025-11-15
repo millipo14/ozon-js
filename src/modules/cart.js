cde7a7bbba72f8cc9472837e6716676452ee20ec
millipo14 <palina517@gmail.com>
2025-11-15 13:17:52 +0300

    Модальное окно + Webpack

---
+++ src/modules/cart.js
@@ -0,0 +1,13 @@
+const cart = () => {
+    const cartBtn = document.getElementById('cart')
+    const cartModal = document.querySelector('.cart')
+    const btnClose = cartModal.querySelector('.cart-close')
+
+    cartBtn.addEventListener('click', () => {
+        cartModal.style.display = 'flex'
+    })
+    btnClose.addEventListener('click', () => {
+        cartModal.style.display = ''
+    })
+}
+export default cart
